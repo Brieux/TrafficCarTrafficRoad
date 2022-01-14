49507f6a3e80a7f7e44db9c330f088fa325541d8
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2022-01-14 10:39:26 +0100

    Rangement

--- Assets/Script/MarquerUnPoint.cs
+++ Assets/Script/MarquerUnPoint.cs
@@ -17,7 +17,7 @@ public class MarquerUnPoint : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
