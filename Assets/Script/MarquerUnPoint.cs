3cae340b4dcd09bbcee0957b066a7d091d00dbe4
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2022-01-19 08:36:01 +0100

    ajout de l'anim ca fonctionne

--- Assets/Script/MarquerUnPoint.cs
+++ Assets/Script/MarquerUnPoint.cs
@@ -6,6 +6,7 @@ public class MarquerUnPoint : MonoBehaviour
 {
     public ParticleSystem NS;
     public int NumAnimToPlay;
+    public bool activated = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,37 +16,44 @@ public class MarquerUnPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!activated)
+        {
+            GetComponent<BoxCollider>().enabled = !GetComponent<BoxCollider>().enabled;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Player")
+        if (!activated)
         {
-            if (gameObject.tag == "End")
+            if (other.gameObject.tag == "Player")
             {
-                GameManager.Instance.newLevel(1);
-            }
-            Debug.Log("Salut");
-            other.gameObject.GetComponent<Score>().Marquer();
-            NS.Play();
-            switch (NumAnimToPlay)
-            {
-                case 1:
-                    other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim1");
-                    break;
+                if (gameObject.tag == "End")
+                {
+                    GameManager.Instance.newLevel(1);
+                }
+                Debug.Log("Salut");
+                other.gameObject.GetComponent<Score>().Marquer();
+                NS.Play();
+                switch (NumAnimToPlay)
+                {
+                    case 1:
+                        other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim1");
+                        break;
 
-                case 2:
-                    other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim2");
-                    break;
-                case 3:
-                    other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim3");
-                    break;
-                default:
-                    other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim1");
-                    break;
+                    case 2:
+                        other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim2");
+                        break;
+                    case 3:
+                        other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim3");
+                        break;
+                    default:
+                        other.gameObject.GetComponent<Animator>().SetTrigger("PlayAnim1");
+                        break;
 
+                }
             }
         }
+        activated = true;
     }
 }
